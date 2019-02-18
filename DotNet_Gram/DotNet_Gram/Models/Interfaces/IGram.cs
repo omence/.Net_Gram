4acda56609560e41109e1865379d2f9af6b19458
Jason Few <omence11@gmail.com>
2019-02-18 09:17:28 -0800

    tests complete

--- DotNet_Gram/DotNet_Gram/Models/Interfaces/IGram.cs
+++ DotNet_Gram/DotNet_Gram/Models/Interfaces/IGram.cs
@@ -7,12 +7,31 @@ namespace DotNet_Gram.Models.Interfaces
 {
     public interface IGram
     {
+        /// <summary>
+        /// delete a post
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         Task Delete(int id);
 
+        /// <summary>
+        /// get all post and display them on index
+        /// </summary>
+        /// <returns></returns>
         Task<List<NetGram>> GetAllNetGrams();
 
+        /// <summary>
+        /// get details of one post and display to manage page
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         Task<NetGram> GetDetails(int id);
 
+        /// <summary>
+        /// save changes or new post
+        /// </summary>
+        /// <param name="netGram"></param>
+        /// <returns></returns>
         Task SaveNetGram(NetGram netGram);
         
     }
--- DotNet_Gram/DotNet_Gram/Startup.cs
+++ DotNet_Gram/DotNet_Gram/Startup.cs
@@ -24,7 +24,6 @@ namespace DotNet_Gram
         {
             var builder = new ConfigurationBuilder().AddEnvironmentVariables();
             builder.AddUserSecrets<Startup>();
-            Configuration = builder.Build();
             Configuration = configuration;
         }
         // This method gets called by the runtime. Use this method to add services to the container.
